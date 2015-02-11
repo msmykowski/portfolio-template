3cc547beb4a5d3035eac6413fc20d1bb792e4a42
msmykowski <smycal@gmail.com>
2015-02-10 16:08:54 -0800

    merge with master

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  this.route("about", { path: "/about" });
+  this.route("about", { path: "/" });
   this.route("portfolio", { path: "/portfolio" });
   this.route("resume", { path: "/resume" });
 });
 
 App.IndexRoute = Ember.Route.extend({
-  
+
 });
